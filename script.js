853374130612dab6a324700ea8dc1eb88a7b1cb1
Garret Crouse <garretlcrouse@icloud.com>
2024-02-10 11:06:53 -0500

    completed js with function

--- script.js
+++ script.js
@@ -4,39 +4,48 @@ var sauce = document.getElementById("sauce").value;
 
 
 // toppings array
-var toppings = [];
+var toppingArray = [];
 
 
 // event listener
 var formButton = document.getElementById("btn");
-btn.addEventListener ("click", function(event) {
+btn.addEventListener("click", function (event) {
     event.preventDefault();
     var topping1 = document.getElementById('top1').value;
     var topping2 = document.getElementById('top2').value;
     var topping3 = document.getElementById('top3').value;
-    toppings += topping1 + topping2 + topping3;
-    calculateTotal(toppings);
+    toppingArray += topping1 + topping2 + topping3;
+    calculateTotal(toppingArray);
 })
 
 
 
 
 
-function calculateTotal(toppingArray) {  
-    let total = 0; 
-    let toppingCost = 2.50; 
+function calculateTotal(toppingArray) {
+    let total = 0;
+    let toppingCost = 2.50;
     let baseCost = 5.50; // cost of crust and sauce
 
     // order string concatenation
-    
+    let orderString = (crust + " pizza with " + sauce);
 
     let toppingString = "Toppings: ";
 
     // For Loop
+    for (let i = 0; i < toppingArray.lenght; i++) {
+        if (toppingArray[i] != "") {
+            total += toppingCost;
+            toppingString += toppingArray[i] + " ";
+        }
+    }
 
     // total = baseCost + cost of all toppings
-
+    total += baseCost;
     // set DOM total += total   
+    document.getElementById("total").innerHTML += total;
     // use DOM: = orderString   
+    document.getElementById("pizzaOrder").innerHTML = orderString;
     // use DOM: = toppingString 
+    document.getElementById("toppings").innerHTML = toppingString;
 }
